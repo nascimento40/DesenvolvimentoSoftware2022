b16a490630cbf89e07ad13ccf3685b1320368b63
nascimento40 <101153749+nascimento40@users.noreply.github.com>
2022-03-30 22:07:32 -0300

    Add files via upload

---
+++ aula4/Tabuada.java
@@ -0,0 +1,14 @@
+public class Tabuada {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.printf("%d * %d = %d \n", i, j, i * j);
+            }
+        }
+    }
+}
+
+//System.out.println("n1="+n1+"n2="+n2+"n3="+n3+)
+//System.out.printf("n1=%d n2=%d n3=%d", n1, n2, n3)
+//\n força uma quebra de linha
+//\t tabulação "Tab"
