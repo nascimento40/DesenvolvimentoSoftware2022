933b42b21d3a78fb45d3cdd99c632fbef4418dca
nascimento40 <101153749+nascimento40@users.noreply.github.com>
2022-05-11 21:41:08 -0300

    Add files via upload

---
+++ area_forma_geometrica/grafica/FrmCalculadoraGeometrica.java
@@ -0,0 +1,10 @@
+package grafica;
+
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class FrmCalculadoraGeometrica extends JFrame{
+    public FrmCalculadoraGeometrica() {
+        setSize(new Dimension(720, 600));
+    }
+}
---
+++ area_forma_geometrica/logica/PreenchimetoFormas.java
@@ -0,0 +1,4 @@
+package logica;
+public interface PreenchimetoFormas {
+    public String preencherCor();
+}
