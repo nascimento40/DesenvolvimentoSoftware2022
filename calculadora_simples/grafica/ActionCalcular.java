04b433f40f1fcb6559f192efafc2415a90e65eaa
nascimento40 <101153749+nascimento40@users.noreply.github.com>
2022-05-18 22:19:32 -0300

    Add files via upload

---
+++ calculadora_simples/grafica/ActionCalcular.java
@@ -0,0 +1,30 @@
+package grafica;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+import logica.Calculadora;
+
+public class ActionCalcular extends AbstractAction{
+    private JTextField txtX, txtY;
+    private JComboBox<String> cbxOperacao;
+    public ActionCalcular(JTextField txtX, JTextField txtY, JComboBox<String> cbxOperacao) {
+        this.txtX = txtX;
+        this.txtY = txtY;
+        this.cbxOperacao = cbxOperacao;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        double x = Double.parseDouble(txtX.getText());
+        double y = Double.parseDouble(txtY.getText());
+        Calculadora calc = new Calculadora(x, y);
+        if (cbxOperacao.getSelectedItem().equals("Soma")) {
+            JOptionPane.showMessageDialog(null, String.format("O resultado de %f + %f é %f", x, y, calc.somar()), "Calculadora Simples", JOptionPane.INFORMATION_MESSAGE);
+        } // fazer para as demais        
+    }
+}
