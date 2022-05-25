8e595f9181482ae2d8c313590b7b8a76d91de459
nascimento40 <101153749+nascimento40@users.noreply.github.com>
2022-05-25 10:19:30 -0300

    Add files via upload

---
+++ calculadora_simples/logica/ProgramaPrincipal.java
@@ -0,0 +1,10 @@
+package logica;
+
+import grafica.FrmCalculadoraSimples;
+
+public class ProgramaPrincipal {
+    public static void main(String[] args) {
+        FrmCalculadoraSimples frm = new FrmCalculadoraSimples();
+        frm.setVisible(true);
+    }
+}
